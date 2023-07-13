d3ad86c649a9e26a2430e635baf0f7859fcee2ab
Ali Khaled <alielsaadany94@gmail.com>
2023-07-13 22:59:31 +0300

    Last Commit

--- js/index.js
+++ js/index.js
@@ -1,36 +1,36 @@
-var qoutes = [
+var quotes = [
 {
-qoute:`"Be yourself; everyone else is already taken."`,
+quote:`"Be yourself; everyone else is already taken."`,
 owner:`- Oscar Wilde`,
 },
 {
-  qoute:`"We don't see things as they are, we see them as we are."`,
+  quote:`"We don't see things as they are, we see them as we are."`,
   owner:`- Anaïs Nin`,
 },
 {
-  qoute:`"Life isn't about finding yourself. Life is about creating yourself."`,
+  quote:`"Life isn't about finding yourself. Life is about creating yourself."`,
   owner:`- George Bernard Shaw`,
 },
 {
-  qoute:`"Always do what you are afraid to do."`,
+  quote:`"Always do what you are afraid to do."`,
   owner:`- Ralph Waldo Emerson`,
 },
 {
-  qoute:`"If you remember me, then I don't care if everyone else forgets."`,
+  quote:`"If you remember me, then I don't care if everyone else forgets."`,
   owner:`- Haruki Murakami`,
 },
 {
-  qoute:`"You only live once, but if you do it right, once is enough."`,
+  quote:`"You only live once, but if you do it right, once is enough."`,
   owner:`― Mae West`,
 },
 {
-  qoute:`"Don’t walk in front of me… I may not follow<br>
+  quote:`"Don’t walk in front of me… I may not follow<br>
   Don’t walk behind me… I may not lead<br>
   Walk beside me… just be my friend"`,
   owner:`- Albert Camus`,
 },
 {
-  qoute:`"Never tell the truth to people who are not worthy of it."`,
+  quote:`"Never tell the truth to people who are not worthy of it."`,
   owner:`― Mark Twain`,
 },
 ];
@@ -39,9 +39,9 @@ var randomNum;
 var lastNum;
 function showQuotes() {
     do {
-      randomNum = Math.floor(Math.random() * qoutes.length);
+      randomNum = Math.floor(Math.random() * quotes.length);
     } while (randomNum == lastNum);
-    document.getElementById('quotesElement').innerHTML = qoutes[randomNum].qoute;
-    document.getElementById('owners').innerHTML = qoutes[randomNum].owner;
+    document.getElementById('quotesElement').innerHTML = quotes[randomNum].quote;
+    document.getElementById('owners').innerHTML = quotes[randomNum].owner;
     lastNum = randomNum;
   }
