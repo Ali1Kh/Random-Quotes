9d700713c8e1d9ab53cc6223a6092e6bfcf1215a
Ali Khaled <alielsaadany94@gmail.com>
2023-12-16 22:41:32 +0200

    Read The Quotes

--- js/index.js
+++ js/index.js
@@ -3,17 +3,29 @@ async function response() {
   let apiRespone = await fetch(`https://api.quotable.io/quotes/random?limit=5`);
   quotes = await apiRespone.json();
 }
-async function main() {
-await response();
-}
-main()
+
+response();
+
 var randomNum;
 var lastNum;
-function showQuotes(){
+function showQuotes() {
   do {
     randomNum = Math.floor(Math.random() * quotes.length);
   } while (randomNum == lastNum);
-  document.getElementById("quotesElement").innerHTML = quotes[randomNum].content;
+  document.getElementById("quotesElement").innerHTML =
+    quotes[randomNum].content;
   document.getElementById("owners").innerHTML = quotes[randomNum].author;
+  document.getElementById("read").style.display = "inline";
   lastNum = randomNum;
+}
+
+document.getElementById("read").addEventListener("click", () => {
+  let text = document.getElementById("quotesElement").innerHTML;
+  readQuote(text);
+});
+
+function readQuote(quote) {
+  let utterance = new SpeechSynthesisUtterance(quote);
+  utterance.pitch = 1.2;
+  speechSynthesis.speak(utterance);
 }
